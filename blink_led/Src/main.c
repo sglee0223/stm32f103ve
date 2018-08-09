e36c5ea35f1530328b055d5b864c2ff558de8a9c
Seung Geun, Lee <sglee0223@gmail.com>
2018-08-09 23:54:55 +0900

    implement blink_led function
            modified:   Src/main.c

--- blink_led/Src/main.c
+++ blink_led/Src/main.c
@@ -105,7 +105,20 @@ int main(void)
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6);
+		HAL_Delay(500);
 
+		HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
+		HAL_Delay(500);
+		
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+		HAL_Delay(500);
+		
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_SET);
+		HAL_Delay(500);
+		
+		HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6|GPIO_PIN_7, GPIO_PIN_RESET);
+		HAL_Delay(500);
   }
   /* USER CODE END 3 */
 
